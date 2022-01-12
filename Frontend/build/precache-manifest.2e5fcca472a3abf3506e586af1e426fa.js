b06a9b6aa1cd5c3880870c8d3ee5bcf6bb356363
Yenifer <guajeyenifer@gmail.com>
2022-01-11 21:56:52 -0500

    Se realiza despliegue del frontend en firebase

---
+++ Frontend/build/precache-manifest.2e5fcca472a3abf3506e586af1e426fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68722be13ec9c3323eec123923a0ac6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1b8943d8e758289d092",
+    "url": "/static/css/2.6a083e77.chunk.css"
+  },
+  {
+    "revision": "d1834fc34bf4a34eb6d9",
+    "url": "/static/css/main.f8ef01b3.chunk.css"
+  },
+  {
+    "revision": "c1b8943d8e758289d092",
+    "url": "/static/js/2.4012426e.chunk.js"
+  },
+  {
+    "revision": "d1834fc34bf4a34eb6d9",
+    "url": "/static/js/main.ef3c02a0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f29cbdcfff554a2f639c94a0e3a47d8b",
+    "url": "/static/media/logo.f29cbdcf.png"
+  }
+]);
